ee5ebcea630d1924151fa9ee8fae32cd609a2bc2
Prajesh Dhande <120330895+prajesh2005@users.noreply.github.com>
2023-12-11 19:21:29 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import React from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
+
+//API... 80f0bc1a
+
+const API_URL = "https://www.omdbapi.com?apikey=80f0bc1a";
+
+const movie1 = {
+  Title: "Italian Spiderman",
+  Year: "2007",
+  imdbID: "tt2705436",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg",
+};
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState([]);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+  return (
+    <div className="app">
+      <h1>MovieSpace</h1>
+      <div className="search">
+        <input
+          placeholder="Search Movie..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
+      </div>
+
+      {
+        // if movie is not found in the api then write no movie found
+        movies?.length > 0 ? (
+          <div className="container">
+            {movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No movies found</h2>
+          </div>
+        )
+      }
+    </div>
+  );
+};
+
+export default App;
